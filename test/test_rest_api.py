e85f94d60c9c5e9813e03c5bb040c2abe069458e
jonni-max <122572277+jonni-max@users.noreply.github.com>
2023-04-30 21:42:06 +0200

    Rerun flake8

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -5,9 +5,9 @@ Tests of the REST API for the POST and GET
 """
 
 
-import pytest
-import json
-import numpy as np
+# import pytest
+# import json
+# import numpy as np
 import pandas as pd
 
 from fastapi.testclient import TestClient
@@ -17,45 +17,52 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_api_get():
-    
+
     r = client.get("http://127.0.0.1:8000/")
-    
+
     print(r.json()[0])
-    
+
     assert r.status_code == 200
-    assert r.json() == ["Welcome to the udacity-mldevops project 3 salary prediction REST API!"]
-    
+    assert r.json() == [
+        "Welcome to the udacity-mldevops project 3 " +
+        "salary prediction REST API!"]
+
 
 def test_api_post_gt50k():
-    
+
     # Take a record from the census data with salary greater than 50k
     census_data = pd.read_csv('data/census_test.csv')
-    input_data = census_data[census_data.salary=='<=50K'].drop(['salary'], axis=1).iloc[1, :]
-    
+    input_data = census_data[census_data.salary ==
+                             '<=50K'].drop(['salary'], axis=1).iloc[1, :]
+
     print(f"\nPOST input is:\n{input_data.to_json()}")
-    
-    r = client.post("http://127.0.0.1:8000/predict/", data=input_data.to_json())
-    
+
+    r = client.post(
+        "http://127.0.0.1:8000/predict/",
+        data=input_data.to_json())
+
     print(f"\nPOST output is:\n{r.json()}")
-    
+
     assert r.status_code == 200
     assert r.json() == {'prediction': ['<=50K']}
-    
+
 
 def test_api_post_lt50k():
-    
+
     # Take a record from the census data with salary less than 50k
     census_data = pd.read_csv('data/census_test.csv')
-    input_data = census_data[census_data.salary=='>50K'].drop(['salary'], axis=1).iloc[1, :]
-    
+    input_data = census_data[census_data.salary ==
+                             '>50K'].drop(['salary'], axis=1).iloc[1, :]
+
     print(f"\nPOST input is:\n{input_data.to_json()}")
-    
-    r = client.post("http://127.0.0.1:8000/predict/", data=input_data.to_json())
-    
+
+    r = client.post(
+        "http://127.0.0.1:8000/predict/",
+        data=input_data.to_json())
+
     print(f"\nPOST output is:\n{r.json()}")
 
     assert r.status_code == 200
     assert r.json() == {'prediction': ['>50K']}
-    
-    
