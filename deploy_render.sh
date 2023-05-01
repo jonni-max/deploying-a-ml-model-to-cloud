b8ad24176937ebb2915e1d1b08761918d7e70a5b
jonni-max <122572277+jonni-max@users.noreply.github.com>
2023-05-01 14:01:07 +0200

    Render update

--- deploy_render.sh
+++ deploy_render.sh
@@ -7,8 +7,8 @@ pip install -r requirements_render.txt &&
 source ./aws_cred_export &&
 mkdir -p './tmp/dvc' &&
 dvc config core.site_cache_dir './tmp/dvc' && 
-dvc pull -vv -r jps3 
-dvc config -vv --list
+dvc pull -r jps3 
+#dvc config -vv --list
 
 echo "DVC pulled data:"
 dvc ls -R --dvc-only ./data
